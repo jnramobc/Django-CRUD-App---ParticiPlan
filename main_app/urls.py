623b0227b993b7b92ceb08a19317cff83135d3ca
Jessica Ramos <jessica.n.ramos@me.com>
2024-09-12 05:28:10 -0400

    added CRUD for events and css for form

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views # Import views to connect routes to view functions
+from .views import VolunteerCreate, VolunteerUpdate, VolunteerDelete, VolunteerDetail
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('volunteers/', views.volunteer_index, name='volunteer-index'),
-    path('volunteers/<int:volunteer_id>/', views.volunteer_detail, name='volunteer-detail'),
+    path('volunteers/<int:pk>/', VolunteerDetail.as_view(), name='volunteer-detail'),
+    path('volunteers/create/', VolunteerCreate.as_view(), name='volunteer-create'),
+    path('volunteers/<int:pk>/update/', VolunteerUpdate.as_view(), name='volunteer-update'),
+    path('volunteers/<int:pk>/delete/', VolunteerDelete.as_view(), name='volunteer-delete'),
     path('events/', views.event_index, name='event-index'),
     path('events/<int:event_id>/', views.event_detail, name='event-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,12 +2,17 @@
 
 from django.shortcuts import render
 
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
 from .models import Event
 
 from .models import Volunteer
+
+from django.urls import reverse_lazy
+
 
 # Define the home view function
 def home(request):
@@ -22,17 +27,41 @@ def event_index(request):
     events = Event.objects.all() 
     return render(request, 'events/index.html', {'events': events})
 
-
+def event_detail(request, event_id):
+    event = Event.objects.get(id=event_id)  
+    return render(request, 'events/detail.html', {'event': event})
 
 def volunteer_index(request):
     volunteers = Volunteer.objects.all()
     return render(request, 'volunteers/index.html', {'volunteers': volunteers})
 
-def event_detail(request, event_id):
-    event = Event.objects.get(id=event_id)  
-    return render(request, 'events/detail.html', {'event': event})
 
+from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from .models import Volunteer
+from django.urls import reverse_lazy
 
-def volunteer_detail(request, volunteer_id):
-    volunteer = Volunteer.objects.get(id=volunteer_id)
-    return render(request, 'volunteers/detail.html', {'volunteer': volunteer})
+# Volunteer Create View
+class VolunteerCreate(CreateView):
+    model = Volunteer
+    fields = ['name', 'email', 'phone_number', 'availability']
+    template_name = 'volunteers/volunteer_form.html'
+    success_url = reverse_lazy('volunteer-index')
+
+# Volunteer Detail View
+class VolunteerDetail(DetailView):
+    model = Volunteer
+    template_name = 'volunteers/detail.html'
+
+# Volunteer Update View
+class VolunteerUpdate(UpdateView):
+    model = Volunteer
+    fields = ['name', 'email', 'phone_number', 'availability']
+    template_name = 'volunteers/volunteer_form.html'
+    success_url = reverse_lazy('volunteer-index')
+
+# Volunteer Delete View
+class VolunteerDelete(DeleteView):
+    model = Volunteer
+    template_name = 'volunteers/volunteer_confirm_delete.html'
+    success_url = reverse_lazy('volunteer-index')
