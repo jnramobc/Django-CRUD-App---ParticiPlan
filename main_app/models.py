a7b1ca2262cc844d3059f5d9732241747b146928
Jessica Ramos <jessica.n.ramos@me.com>
2024-09-12 02:37:00 -0400

    created models, did migrations, and created first instances of database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+# User Model
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
+
+# Volunteer Model
+class Volunteer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    availability = models.CharField(max_length=100, blank=True, null=True)
+   
+
+    def __str__(self):
+        return self.name
+
+# Event Model
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    location = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    
+
+    def __str__(self):
+        return self.title
+
+
+
--- participlan/settings.py
+++ participlan/settings.py
@@ -95,10 +95,11 @@ if 'ON_HEROKU' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'participlan',  # Your database name
     }
 }
+
 
 
 # Password validation
@@ -136,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
